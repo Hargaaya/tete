98aa67c2436b4c0165c436636ed47d61b7a2ce5b
Hargaaya Idris <hargaaya10@gmail.com>
2026-01-31 14:12:07 +0100

    feat: bootstrap project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Têtê
+
+A game inspired by [Heads Up!](https://www.warnerbros.com/games-and-apps/heads)
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Têtê</h1>
+      <p className="mt-4 text-gray-400">A "Heads Up!" inspired game, for fun times with friends.</p>
+    </div>
+  );
+}
